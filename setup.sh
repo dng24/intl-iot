232b149577e679b062fb8c7f50434c83c82556f8
dng24 <37806620+dng24@users.noreply.github.com>
2020-08-27 13:35:35 -0400

    add missing dependency

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ pip install numpy scipy pyshark geoip2 matplotlib dpkt pycrypto IPy pcapy scapy 
     mysql-connector-python-rf pandas tldextract python-whois ipwhois psutil seaborn
 
 #for model pipeline
-pip install 'certifi==2020.6.20' 'joblib==0.16.0' 'numpy==1.19.1' 'pandas==1.1.0' \
+pip install 'statsmodels==0.12.0' 'certifi==2020.6.20' 'joblib==0.16.0' 'numpy==1.19.1' 'pandas==1.1.0' \
     'python-dateutil==2.8.1' 'pytz==2020.1' 'scikit-learn==0.23.2' 'scipy==1.5.2' \
     'six==1.15.0' 'threadpoolctl==2.1.0' 'seaborn==0.10.1' 'whois==0.9.7'
 
