aa28467f90d37abd7cd787e7b1e79f4fa2f009a0
Derek Ng <derekjng@gmail.com>
2020-08-27 10:41:45 -0400

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#setup python3.6 environment
+sudo apt-get install virtualenv libpcap-dev libpq-dev python3-dev python3.6-tk gcc tshark
+virtualenv -p python3.6 env
+source env/bin/activate
+
+#for all pipelines
+pip install numpy scipy pyshark geoip2 matplotlib dpkt pycrypto IPy pcapy scapy Impacket \
+    mysql-connector-python-rf pandas tldextract python-whois ipwhois psutil seaborn
+
+#for model pipeline
+pip install 'certifi==2020.6.20' 'joblib==0.16.0' 'numpy==1.19.1' 'pandas==1.1.0' \
+    'python-dateutil==2.8.1' 'pytz==2020.1' 'scikit-learn==0.23.2' 'scipy==1.5.2' \
+    'six==1.15.0' 'threadpoolctl==2.1.0' 'seaborn==0.10.1' 'whois==0.9.7'
+
+deactivate
