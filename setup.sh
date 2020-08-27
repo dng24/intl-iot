92c54ad0e2cfb3424e0a1bf0b74eededa8c9430e
dng24 <37806620+dng24@users.noreply.github.com>
2020-08-27 14:20:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,11 @@ virtualenv -p python3.6 py3.6
 source py3.6/bin/activate
 
 #for all pipelines
-pip install numpy scipy pyshark geoip2 matplotlib dpkt pycrypto IPy pcapy scapy Impacket \
-    mysql-connector-python-rf pandas tldextract python-whois ipwhois psutil seaborn
+pip install 'numpy==1.19.1' 'scipy==1.5.2' pyshark geoip2 matplotlib dpkt pycrypto IPy pcapy \
+    scapy Impacket mysql-connector-python-rf 'pandas==1.1.0' tldextract python-whois ipwhois psutil
 
 #for model pipeline
-pip install 'statsmodels==0.12.0' 'certifi==2020.6.20' 'joblib==0.16.0' 'numpy==1.19.1' 'pandas==1.1.0' \
-    'python-dateutil==2.8.1' 'pytz==2020.1' 'scikit-learn==0.23.2' 'scipy==1.5.2' \
-    'six==1.15.0' 'threadpoolctl==2.1.0' 'seaborn==0.10.1' 'whois==0.9.7'
+pip install 'statsmodels==0.12.0' 'certifi==2020.6.20' 'joblib==0.16.0' 'python-dateutil==2.8.1' \
+    'pytz==2020.1' 'scikit-learn==0.23.2' 'six==1.15.0' 'threadpoolctl==2.1.0' 'seaborn==0.10.1'
 
 deactivate
